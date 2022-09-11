ab9ee1eddda3e17cd39e6c216e6d8247596e1063
June Roh <texasroh@gmail.com>
2022-09-11 04:46:30 -0400

    install themmoviedb api

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,12 @@
+import { useQuery } from "react-query";
+import { getMovies } from "../api";
+
 function Home() {
-    return (
-        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>
-            home
-        </div>
-    );
+  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  console.log(data, isLoading);
+  return (
+    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+  );
 }
 
 export default Home;
---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "c183a5884681c02c32b9c0dad01728ba";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
